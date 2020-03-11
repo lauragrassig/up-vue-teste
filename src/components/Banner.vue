88e5582dbbe3bb93f8e4894a7c19a30343bbc417
Laura Grassi <lgrassi@NBUPLEX-470.uplexis.local>
2020-03-11 11:08:49 -0300

    banner finalizado

---
+++ src/components/Banner.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="wrapper_banner">
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      background="#d6d6d6"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide img-blank img-alt="Blank image">
+        <div class="banner">
+          <div class="banner_title">
+            <div class="logo--banner">
+              <div class="logo_gota"></div>
+              <div class="logo_name"><h1>flooop</h1></div>
+            </div>
+            <div class="title_title">
+              <h2 class="title_banner">Histórico Empresarial</h2>
+            </div>
+          </div>
+          <div class="banner_description">
+            <p>O aplicativo Histórico Empresarial permite ao usuário ter acesso a todos os fatos e acontecimentos relevantes de uma empresa desde o seu ano de fundação</p>
+          </div>
+          <div class="banner_price">
+            <div class="price_price">
+              <span>R$</span>
+              <span>40,00</span>
+            </div>
+            <div class="price_button">
+              <button class="button_default">Saiba Mais</button>
+            </div>
+          </div>
+        </div>
+      </b-carousel-slide>
+      <b-carousel-slide img-blank img-alt="Blank image">
+        <div class="banner">
+          <div class="banner_title">
+            <div class="logo--banner">
+              <div class="logo_gota"></div>
+              <div class="logo_name"><h1>flooop</h1></div>
+            </div>
+            <div class="title_title">
+              <h2 class="title_banner">Histórico Empresarial</h2>
+            </div>
+          </div>
+          <div class="banner_description">
+            <p>O aplicativo Histórico Empresarial permite ao usuário ter acesso a todos os fatos e acontecimentos relevantes de uma empresa desde o seu ano de fundação</p>
+          </div>
+          <div class="banner_price">
+            <div class="price_price">
+              <span>R$</span>
+              <span>40,00</span>
+            </div>
+            <div class="price_button">
+              <button class="button_default">Saiba Mais</button>
+            </div>
+          </div>
+        </div>
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banner/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Banner from '@/components/Banner.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Banner
   }
 }
 </script>
