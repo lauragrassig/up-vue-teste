052e4e26c22c4e2e92637575eb8c8c05ea9a151f
lauragrassig <laura_grassig@outlook.com>
2020-03-10 22:42:41 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <router-link to="/">Home</router-link>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <router-link to="/about">About</router-link>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Bootstrap
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Styles
+import style from './assets/scss/style.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
+  style,
   render: h => h(App)
 }).$mount('#app')
